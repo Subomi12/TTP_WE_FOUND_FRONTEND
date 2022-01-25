cde340fa5a550d793f87d1e9aa3391f6112d7c35
Subomi12 <IDRIS.ADEBOYEJO66@bcmail.cuny.edu>
2022-01-24 23:03:39 -0500

    created login

---
+++ wefound/src/Components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Login(){
+    
+    return(
+        <div>
+            <h2>Login</h2>
+            <form>
+                <label>Username: </label>
+                <input type = "text" /><br></br>
+                <label>Password: </label>
+                <input type = "text"/><br></br>
+                <button type = "submit">Submit</button>
+            </form>
+        </div>
+    )
+}
